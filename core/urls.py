cb0ba0dea3a784347dddf9eec100c468270bd1de
shimul <shimul929@gmail.com>
2023-01-02 22:20:08 +0600

    gull added345

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, re_path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('intro', views.index),
+    path('signin', views.index),
+    # any link that starts with dashboard/
+    re_path(r'^dashboards/', views.index),
     path('dashboard/', views.dashboard),
     path('administrator/', views.administrator),
     re_path(r'^dashboard/(?P<slug>[\w-]+)/$', views.dashboard),
